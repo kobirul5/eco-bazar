eaff970e5f6552cab8ff387ac315c802ac5ef474
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-02-18 01:48:38 +0600

    popular tag and rating section

--- src/components/home/SideBarComponents/PopularTag.jsx
+++ src/components/home/SideBarComponents/PopularTag.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
 
 export default function PopularTag() {
+
+  const items = [
+    "Healthy",
+    "Low fat",
+    "Vegetarian",
+    "Kid foods",
+    "Vitamins",
+    "Bread",
+    "Meat",
+    "Snacks",
+    "Tiffin",
+    "Launch",
+    "Dinner",
+    "Breacktast",
+    "Fruit"
+  ];
+
   return (
-    <div>PopularTag</div>
+    <div>
+      <h3 className="font-bold text-xl">Populer Tag</h3>
+        <div className="gap-4 flex flex-wrap mt-4">
+          {
+            items?.map((i,idx)=><button className="btn btn-xs px-3 rounded-full" key={idx}>{i}</button>)
+          }
+        </div>
+    </div>
   )
 }
--- src/components/home/SideBarComponents/Rating.jsx
+++ src/components/home/SideBarComponents/Rating.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
 export default function Rating() {
   return (
-    <div>Rating</div>
+    <div>
+      <h3 className="font-bold text-xl">Rating</h3>
+      <div className='flex flex-col gap-2'>
+        <div className='flex gap-2 text-4xl'>
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+        </div>
+        <div className='flex gap-2 text-4xl'>
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-gray-300" />
+        </div>
+        <div className='flex gap-2 text-4xl'>
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+        </div>
+        <div className='flex gap-2 text-4xl'>
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+        </div>
+        <div className='flex gap-2 text-4xl'>
+          <FaStar className="text-orange-500" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+          <FaStar className="text-gray-300" />
+        </div>
+      </div>
+    </div>
   )
 }
